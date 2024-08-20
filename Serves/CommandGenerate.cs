a5f913328e52d8a7ebfc3300fbee989c7a230102
jianlingg <yunjicangtian@gmail.com>
2024-08-20 15:37:15 +0800

    DAC波形发生器控制

--- Serves/CommandGenerate.cs
+++ Serves/CommandGenerate.cs
@@ -35,9 +35,104 @@
 
 
         //FX3控制指令字符串生成
-        static string FX3CmdGen(int target, string args)
+        static public string FX3CmdGen(int target, string args)
         {
             return $"A{target.ToHex(2)}{(args.Length / 8).ToHex(5)}{args}";
+        }
+
+        //DAC波形配置指令生成
+        static public string WaveConfig(string Data, string Range, string Count = "1")
+        {
+            var range = Range.Matches(@"\d+");
+            var output = string.Empty;
+            for (var i = 0; i < range.Count(); i = i + 2)
+            {
+                output = output + $"{"2".DecToBin(4)}{range[i].DecToBin(14)}{range[i + 1].DecToBin(14)}".BinToHex(8);
+            }
+
+            var count = $"{"3".DecToBin(4)}{Count.DecToBin(28)}".BinToHex(8);
+
+            var start = $"{"4".DecToBin(4)}{"0".DecToBin(28)}".BinToHex(8);
+
+            return Data + output + count + start;
+        }
+
+        //DAC波形数据指令生成
+        static public string WaveGenerate(List<int> input)
+        {
+            var output = string.Empty;
+            for (var i = 0; i < input.Count(); i = i + 2)
+            {
+                output = output + $"{"1".DecToBin(4)}{input[i].ToBin(14)}{input[i + 1].ToBin(14)}".BinToHex(8);
+            }
+            return output;
+        }
+
+        //DAC三角波数据指令生成
+        static public List<int> Sawtooth(int t = 10, double width = 0.5, double Amplitude = 1.0)
+        {
+            // 检查参数有效性
+            if (t <= 0)
+                throw new ArgumentException("Number of points must be positive.");
+            if (width <= 0 || width >= 1)
+                throw new ArgumentException("Width must be between 0 and 1.");
+
+            var x = Enumerable.Range(0, t).Select(i => (double)i / t).ToList(); // 生成归一化的时间向量
+            var waveform = x.Select(xi =>
+            {
+                double sawtoothValue = (xi % 1.0) / width;
+                if (sawtoothValue >= 1.0)
+                    sawtoothValue = 2.0 - sawtoothValue;
+                return (int)Math.Round(Amplitude * 16383 * sawtoothValue);
+            }).ToList();
+
+            return waveform;
+        }
+
+        //DAC正弦波数据指令生成
+        static public List<int> Sinwave(int t = 10, double Amplitude = 1.0)
+        {
+            // 检查参数有效性
+            if (t <= 0)
+                throw new ArgumentException("Number of points must be positive.");
+
+            var x = Enumerable.Range(0, t);// 生成归一化的时间向量
+
+            double step = 2 * Math.PI / (t - 1); // 归一化(归2Pi化)
+
+            var waveform = x.Select(i =>
+            {
+                double x = i * step;
+                double y = Amplitude * 16383 * (Math.Sin(x) + 1) / 2;
+                return (int)Math.Round(y);
+            }).ToList();
+
+            return waveform;
+        }
+
+        //波形编辑器数据指令生成
+        static public List<int> Formula(string input)
+        {
+            var input2 = input.Remove(@"\s")
+                          .Split(",")
+                          .Select(x => x.Split(":"))
+                          .ToArray();
+            var Lists = new List<int>();
+            foreach (var item in input2)
+            {
+                var tmp = item[0].Split("-").Select(x => x.ToInt()).ToArray();
+                var dif = tmp[1] - tmp[0] + 1;
+                var e = new NCalc.Expression(item[1]);
+
+                var x = Enumerable.Range(0, dif)
+                                  .Select(x =>
+                                  {
+                                      e.Parameters["x"] = x;
+                                      return e.Evaluate().ToString().ToInt();
+                                  }).ToList();
+                Lists.AddRange(x);
+            }
+            return Lists;
         }
     }
 }
---
+++ ViewModels/WavePageViewModel.cs
@@ -0,0 +1,89 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using Test1.Serves;
+
+namespace Test1.ViewModels
+{
+    public partial class WavePageViewModel : ObservableRecipient
+    {
+
+        [ObservableProperty]
+        public string? _waveRange = "0-99";
+
+        [ObservableProperty]
+        public string? _waveCount = "1";
+
+        [ObservableProperty]
+        public int? _Saw_t = 100;//点的数量
+
+        [ObservableProperty]
+        public double? _Saw_width = 0.5;
+
+        [ObservableProperty]
+        public double? _Saw_Amplitude = 1.0;
+
+        [ObservableProperty]
+        public int? _Sin_t = 10;
+
+        [ObservableProperty]
+        public double? _Sin_Amplitude = 1.0;
+
+        [ObservableProperty]
+        public string? _waveFormula = "0-99:16200, 100-299:0, 300-499:10*x";
+
+        
+
+        [RelayCommand]
+        private void SinGen()
+        {
+            var myDevice = new FX3DataHandle().FX3Device();
+
+            if (myDevice == null || Sin_t == null || Sin_Amplitude == null)
+                return;
+
+            var sinWave = CommandGenerate.Sinwave(t: (int)Sin_t!, Amplitude: (double)Sin_Amplitude!);
+            var sinData = CommandGenerate.WaveGenerate(sinWave);
+            var wave = CommandGenerate.WaveConfig(sinData, WaveRange!, WaveCount!);
+            var cmd = CommandGenerate.FX3CmdGen(2, wave);
+
+            new FX3DataHandle().FX3DataOut(myDevice, cmd!);
+
+        }
+
+        [RelayCommand]
+        private void SawGen()
+        {
+            var myDevice = new FX3DataHandle().FX3Device();
+
+            if (myDevice == null || Saw_t == null || Saw_width == null || Saw_Amplitude == null)
+                return;
+
+            var sawWave = CommandGenerate.Sawtooth(t: (int)Saw_t!, width: (double)Saw_width!, Amplitude: (double)Saw_Amplitude!);
+            var sawData = CommandGenerate.WaveGenerate(sawWave);
+            var wave = CommandGenerate.WaveConfig(sawData, WaveRange!, WaveCount!);
+            var cmd = CommandGenerate.FX3CmdGen(2, wave);
+
+            new FX3DataHandle().FX3DataOut(myDevice, cmd!);
+
+        }
+
+        [RelayCommand]
+        private void WaveGen()
+        {
+            var myDevice = new FX3DataHandle().FX3Device();
+
+            if (myDevice == null || WaveFormula == null)
+                return;
+
+            var FormulaWave = CommandGenerate.Formula(WaveFormula!);
+            var FormulaData = CommandGenerate.WaveGenerate(FormulaWave);
+            var wave = CommandGenerate.WaveConfig(FormulaData, WaveRange!, WaveCount!);
+            var cmd = CommandGenerate.FX3CmdGen(2, wave);
+
+            new FX3DataHandle().FX3DataOut(myDevice, cmd!);
+
+        }
+
+    }
+}
